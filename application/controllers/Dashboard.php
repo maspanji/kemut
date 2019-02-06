5de671067a8b61e645979eeba32a1997fc122f16
panji <maspanji@gmail.com>
2019-02-06 11:09:18 +0700

    pemisahan header dan footer untuk prototype dan perubahan konfigurasi.

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,7 +5,9 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view("dashboard");
+            $this->load->view("proto/head");
+            $this->load->view("dashboard");
+            $this->load->view("proto/foot");
 	}
 
 }
